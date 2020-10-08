cbbdff5607e5e985948cc1cc793c5529615a245d
GerssonRoR <61944709+GerssonRoR@users.noreply.github.com>
2020-10-08 14:01:22 -0500

    *Primeros cambios

--- app/Models/GeneralFunctions.php
+++ app/Models/GeneralFunctions.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+<<<<<<< HEAD
 Use Dotenv\Dotenv;
 require_once('BasicModel.php');
+=======
+use Dotenv\Dotenv;
+>>>>>>> *Primeros cambios
 
 require(__DIR__ .'/../../vendor/autoload.php');
 class GeneralFunctions
